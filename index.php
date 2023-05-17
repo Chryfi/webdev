aa5db8a2228478403920fc79959b8d419c15df17
Chryfi <71967555+Chryfi@users.noreply.github.com>
2023-05-17 13:48:43 +0200

    Finish routing system

--- index.php
+++ index.php
@@ -10,26 +10,30 @@ if (empty($page)) {
 }
 
 $pageFileName = pathinfo($page, PATHINFO_FILENAME);
+$pageFileType = pathinfo($page, PATHINFO_EXTENSION);
+$pageFileType = !empty($pageFileType) ? $pageFileType : findFileType("content/pages/".$dir.$pageFileName, ["html", "php"]);
+$pagePath = "content/pages/".$dir.$pageFileName.".".$pageFileType;
 
-
-$headFiletype = nextFileType("content/heads/".$dir.$pageFileName, ["html", "php"]);
+$headFiletype = findFileType("content/heads/".$dir.$pageFileName, ["html", "php"]);
 $headFile = !$headFiletype ? "index.html" : $pageFileName.".".$headFiletype;
 $headPath = "content/heads/".$dir.$headFile;
 
+/**
+ * If the user tries to enter a directory that doesn't exist in "heads",
+ * the default head file from the base directory needs to be used.
+ * "content/heads/index.html" MUST ALWAYS EXIST!
+ */
 if (file_exists($headPath)) {
-    include("content/heads/".$dir.$headFile);
-} else {
+    include($headPath);
+}
+else {
     include("content/heads/index.html");
 }
 
-
 include("content/defaults/header.php");
 
-$pageFileType = pathinfo($page, PATHINFO_EXTENSION);
-$pageFileType = !empty($pageFileType) ? $pageFileType : nextFileType("content/pages/".$dir.$pageFileName, ["html", "php"]);
-
-if (file_exists("content/pages/".$dir.$pageFileName.".".$pageFileType)) {
-    include("content/pages/".$dir.$pageFileName.".".$pageFileType);
+if ($pageFileType && file_exists($pagePath)) {
+    include($pagePath);
 }
 else {
     $_POST["error_code"] = 404;
@@ -43,7 +47,7 @@ include("content/defaults/footer.php");
 /**
  * @return string|boolean returns the first found filetype in the path. If nothing was found returns false.
  */
-function nextFileType($path, $filetypes) {
+function findFileType($path, $filetypes) {
     for ($i = 0; $i < count($filetypes); $i++) {
         $filetype = $filetypes[$i];
 
