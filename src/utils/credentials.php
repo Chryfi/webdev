7f6e76240191363388fc69de7a2b5ad84bcea207
Chryfi <71967555+Chryfi@users.noreply.github.com>
2023-05-28 21:22:10 +0200

    Add credentials.php to read sensitive data from a file

---
+++ src/utils/credentials.php
@@ -0,0 +1,36 @@
+<?php
+    /**
+     * Functions to read login data from a file.
+     */
+     class Credentials {
+        public static function getDatabasePassword() : ?string {
+            $creds = Credentials::readCredentials();
+            if ($creds == null
+            || !array_key_exists("database", $creds)
+            || !array_key_exists("password", $creds["database"])) {
+                return null;
+            }
+
+            return $creds["database"]["password"];
+        }
+
+        public static function getDatabaseUser() : ?string {
+            $creds = Credentials::readCredentials();
+
+            if ($creds == null
+            || !array_key_exists("database", $creds)
+            || !array_key_exists("username", $creds["database"])) {
+                return null;
+            }
+
+            return $creds["database"]["username"];
+        }
+
+        private static function readCredentials() : mixed {
+            $contents = file_get_contents("../../credentials");
+            $json_data = json_decode($contents, true);
+
+            return $json_data;
+        }
+    }
+?>
