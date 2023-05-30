f1bccc6464b2bf7dee42def6c17e9774072eb6ba
Chryfi <71967555+Chryfi@users.noreply.github.com>
2023-05-30 20:40:09 +0200

    Add BASE_PATH constant to index.php

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
+/* to avoid endless and annoying relative paths */
+define('BASE_PATH', "C:/xampp/htdocs/KatzenBlog");
 
 $request = $_GET['p'];
 $reqComponents = explode('/', $request);
 $page = array_pop($reqComponents);
 $dir = implode('/', $reqComponents) . (count($reqComponents) != 0 ? "/" : "");
+
+/* request to php source files should be let through */
+if (count($reqComponents) != 0 && $reqComponents[0] == "src") {
+    include($request);
+    return;
+}
 
 if (empty($page)) {
     $page = "index";
@@ -45,9 +53,9 @@ include("content/defaults/footer.php");
 
 
 /**
- * @return string returns the first found filetype in the path. If nothing was found returns NULL.
+ * @return ?string returns the first found filetype in the path. If nothing was found returns NULL.
  */
-function findFileType($path, $filetypes) {
+function findFileType($path, $filetypes): ?string {
     for ($i = 0; $i < count($filetypes); $i++) {
         $filetype = $filetypes[$i];
 
