b42d421ba43d404120e2d7c53eae4b0c78e1e91c
Chryfi <71967555+Chryfi@users.noreply.github.com>
2023-06-08 12:48:53 +0200

    Move getPagingHTML to displayBeitrag.php

--- content/pages/katzegorien.php
+++ content/pages/katzegorien.php
@@ -47,47 +47,6 @@ if ($searchText != "" || $searchTitle != "" || count($tags) > 0) {
     $db->disconnect();
 }
 
-function getPagingHTML(int $countSearchResults, int $limit, int $limitPaging, array $getParameter) : string {
-    unset($getParameter["page"]);
-    $currentPage = isset($_GET["page"]) && $_GET["page"] > 0 ? $_GET["page"] : 1;
-    $maxCountPages = ceil($countSearchResults / $limit);
-    $minPage = min(max($currentPage - floor($limitPaging / 2) + 1, 1), $maxCountPages);
-    $maxPage = $minPage + $limitPaging - 1;
-
-    if ($maxPage > $maxCountPages) {
-        $minPage = max($minPage - ($maxPage - $maxCountPages), 1);
-    }
-
-    $maxPage = min($maxPage, $maxCountPages);
-
-    $pagingHTML = '<div class="paging-row">';
-    $i = $minPage;
-
-    if ($currentPage > 1) {
-        $pagingHTML .= '<a href="katzegorien?page='.($currentPage - 1).'&'.http_build_query($getParameter).'">Zurück</a>';
-    }
-
-    $pagingHTML .= '<div class="page-links-row">';
-    while ($i <= $maxPage) {
-        if ($currentPage == $i) {
-            $pagingHTML .= '<a class="active">'.$i.'</a>';
-        } else {
-            $pagingHTML .= '<a href="katzegorien?page='.$i.'&'.http_build_query($getParameter).'">'.$i.'</a>';
-        }
-
-        $i++;
-    }
-    $pagingHTML .= '</div>';
-
-    if ($currentPage < $maxPage) {
-        $pagingHTML .= '<a href="katzegorien?page='.($currentPage + 1).'&'.http_build_query($getParameter).'">Weiter</a>';
-    }
-
-    $pagingHTML .= '</div>';
-
-    return $pagingHTML;
-}
-
 function getSearchCountHTML(int $totalCount, int $currentPage) : string {
     $ergebnisString = $totalCount > 1 ? " Ergebnisse" : " Ergebnis";
     $pageString = $currentPage > 1 ? "Seite ".$currentPage." " : "";
