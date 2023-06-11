ac0bbb905167d0e35cadc5ca23ffcfda8948b6c9
Chryfi <71967555+Chryfi@users.noreply.github.com>
2023-06-11 18:24:43 +0200

    use null coalescing operator in error message page

--- content/pages/error_message.php
+++ content/pages/error_message.php
@@ -1,8 +1,8 @@
 <div class="page-wrapper">
     <header class="container-sm">
         <div class="header-text">
-            <h1>Error <?php if (isset($_POST["error_code"])) echo $_POST["error_code"]; ?></h1>
-            <p><?php if (isset($_POST["error_description"])) echo $_POST["error_description"]; ?></p>
+            <h1>Error <?php echo ($_POST["error_code"] ?? "Error ..."); ?></h1>
+            <p><?php echo ($_POST["error_description"] ?? ""); ?></p>
         </div>
     </header>
 </div>
